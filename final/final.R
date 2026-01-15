8827bdc704a92513881229d62339788e9ed7b288
Kimberly Nicholls <1649528+kielni@users.noreply.github.com>
2026-01-15 14:45:54 -0800

    both plots

--- final/final.R
+++ final/final.R
@@ -1,122 +1,141 @@
-library(tidycensus)
 library(censusapi)
-
-library(tidycensus)
+library(cowplot)
 library(dplyr)
-library(tidyr)
 library(sf)
+library(tidyr)
+library(tidycensus)
 library(tigris)
-library(tmap)
+
 options(tigris_use_cache = TRUE)
 
 # Commute times in the Bay Area, 2000 vs 2019 ----------------------------
 # Data sources
 #   - ACS: B08303	Travel Time To Work
-#   - 2000 Decennial Census: P031
+#   - 2000 Decennial Census:P031 Travel time to work for workers 16 years & over
 
 # Shared code ----------------------------
-# clean <- function(df) {
 
+prep_data <- function(commute) {
+  # prepare commute data with common steps for all years
+  # return dataframe with mean commute time by tract
+
+  # coerce to numeric and drop NAs
+  commute <- commute |>
+    mutate(
+      mean_travel_time = as.numeric(.data$mean_travel_time)
+    ) |>
+    filter(!is.na(.data$mean_travel_time))
+}
+
+plot_data <- function(commute_data, common_scale, year) {
+  # create ggplot object for commute data
+  print(paste("plotting year", year))
+  plt <- ggplot(commute_data) +
+    geom_sf(aes(fill = .data$mean_travel_time), color = NA) +
+    scale_fill_viridis_c(limits = common_scale) +
+    theme_minimal() +
+    labs(
+      title = paste(year),
+      fill = "Mean Commute Time (minutes)"
+    )
+  print(plt)
+  filename <- paste0("commute_", year, ".png")
+  print(paste("writing", filename))
+  ggsave(
+    filename,
+    path = "final",
+    plot = plt,
+    width = 10,
+    height = 6
+  )
+  plt
+}
 
 # Setup ----------------------------
 bay_counties <- c(
   "Alameda", "Contra Costa", "Marin", "Napa", "San Francisco",
   "San Mateo", "Santa Clara", "Solano", "Sonoma"
 )
+year_latest <- 2019
+year_prev <- 2000
 
-# load data for 2019 census tracts and rename variables
+# Prep 2019 data ----------------------------
+# from ACS subject table
+# S0801_C01_046 =
+#   Estimate!!Total!!
+#   Workers 16 years and over who did not work from home!!
+#   TRAVEL TIME TO WORK!!
+#   Mean travel time to work (minutes)
 commute_latest <- get_acs(
   geography = "tract",
   state = "CA",
   county = bay_counties,
-  # the bucket labels are irregular and don't make good variable names,
-  # so be explicit when renaming
-  variables = c(
-    range_0_5 = "B08303_002",
-    range_5_9 = "B08303_003",
-    range_10_14 = "B08303_004",
-    range_15_19 = "B08303_005",
-    range_20_24 = "B08303_006",
-    range_25_29 = "B08303_007",
-    range_30_34 = "B08303_008",
-    range_35_39 = "B08303_009",
-    range_40_44 = "B08303_010",
-    range_45_59 = "B08303_011",
-    range_60_89 = "B08303_012",
-    range_90_plus = "B08303_013"
-  ),
-  year = 2019,
+  survey = "acs5",
+  variables = "S0801_C01_046",
+  year = year_latest,
   geometry = TRUE,
   cache_table = TRUE,
-  survey = "acs5",
-)
+) |>
+  rename(mean_travel_time = estimate)
 
-# set range_0_5, range_5_9, ..., range_90_plus as numeric and drop NAs
-commute_latest <- commute_latest |>
-  mutate(
-    estimate = as.numeric(estimate)
-  ) |>
-  filter(!is.na(estimate))
+mean_commute_latest <- prep_data(commute_latest)
 
-# want consistent categories between 2000 and 2019, so set cutoffs manually
-commute_latest <- commute_latest |>
+# Prep 2000 data ----------------------------
+# from SF3 transportation profile
+# P033001 =
+#   Aggregate travel time to work (in minutes)
+#   by travel time to work
+#   by means of transportation to work
+#   for workers 16 years and over
+#   who did not work at home
+# P031002 =
+#   Total!!Did not work at home
+#   TRAVEL TIME TO WORK FOR WORKERS 16 YEARS AND OVER
+commute_prev <- get_decennial(
+  geography = "tract",
+  state = "CA",
+  county = bay_counties,
+  variables = c(
+    total_time = "P033001",
+    total_workers = "P031002"
+  ),
+  sumfile = "sf3",
+  year = year_prev,
+  geometry = TRUE,
+  cache_table = TRUE,
+  output = "wide"
+) |>
+  # calculate mean travel time
   mutate(
-    category = case_when(
-      # 0-14 minutes = short
-      variable %in% c(
-        "range_0_5", "range_5_9", "range_10_14"
-      ) ~ "short",
-      # 15-34 minutes = medium
-      variable %in% c(
-        "range_15_19", "range_20_24", "range_25_29", "range_30_34",
-        "range_35_39"
-      ) ~ "medium",
-      # 35+ minutes = long
-      variable %in% c(
-        "range_40_44", "range_45_59",
-        "range_60_89", "range_90_plus"
-      ) ~ "long",
-      TRUE ~ "total"
-    )
-  ) |>
-  st_transform(3857)
-
-# set midpoint 2.5 = for variable = range_0_5
-# TODO: test values for 90+
-midpoints <- c(
-  range_0_5 = 2.5, range_5_9 = 7, range_10_14 = 12,
-  range_15_19 = 17, range_20_24 = 22, range_25_29 = 27,
-  range_30_34 = 32, range_35_39 = 37, range_40_44 = 42,
-  range_45_59 = 52, range_60_89 = 75, range_90_plus = 100
-)
-# set midpoint based on variable
-commute_latest <- commute_latest |>
-  mutate(
-    midpoint = midpoints[variable]
+    mean_travel_time = total_time / total_workers
   )
 
-# calculate mean commute time by tract
-mean_commute <- commute_latest |>
-  filter(variable != "total") |>
-  group_by(GEOID, NAME, geometry) |>
-  summarise(
-    mean_minutes =
-      sum(estimate * midpoint, na.rm = TRUE) / sum(estimate, na.rm = TRUE),
-    .groups = "drop"
-  )
+mean_commute_prev <- prep_data(commute_prev)
+
+# Create maps ----------------------------
+# create a common scale so colors will be the same on both plots
+common_scale <- range(
+  c(
+    mean_commute_prev$mean_travel_time,
+    mean_commute_latest$mean_travel_time
+  ),
+  na.rm = TRUE
+)
+plot_prev <- plot_data(mean_commute_prev, common_scale, year_prev)
+plot_latest <- plot_data(mean_commute_latest, common_scale, year_latest)
+# plot side by side
+combined_plot <- plot_grid(plot_prev, plot_latest, ncol = 2)
+ggsave("commute_comparison.png",
+  path = "final",
+  plot = combined_plot,
+  width = 16,
+  height = 6
+)
+# TODO:
+# single legend for both plots
+# crop to urban area boundary
 
 # visualization options
 # simplest: mean commute time color scale by tract
 # more complex: bivariate choropleth with commute time buckets and urban area
 #   - don't use bubbles for mean commute time since it's hard to compare areas
-plt <- ggplot(mean_commute) +
-  geom_sf(aes(fill = mean_minutes), color = NA) +
-  scale_fill_viridis_c(option = "plasma") +
-  theme_minimal() +
-  labs(
-    title = "Mean Commute 2019",
-    fill = "Mean Commute Time (minutes)"
-  )
-print(plt)
-ggsave("commute_2019.png", path = "final", plot = plt, width = 10, height = 6)
-
